0248429e3c2450a48d6ea40e25c26f6c5ec7d6ac
AVue2 <Vuey00@gmail.com>
2025-04-08 22:10:07 -0400

    Modified server.ts file to fix deployment issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,10 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { authenticateToken } from './services/auth.js';
 import { typeDefs, resolvers } from './schemas/index.js';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
