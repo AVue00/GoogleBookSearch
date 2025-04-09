c08503642c427f1796d85537175dddb8681fb6c1
AVue2 <Vuey00@gmail.com>
2025-04-08 21:48:36 -0400

    Cleaned up code

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -3,9 +3,8 @@ import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+
 import Auth from '../utils/auth';
-import type { User } from '../models/User';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
--- client/src/pages/SavedBooks.tsx
+++ client/src/pages/SavedBooks.tsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from 'react';
+
 import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
-import type { User } from '../models/User';
 import { useMutation, useQuery } from '@apollo/client';
 import { REMOVE_BOOK } from '../utils/mutations';
 import { GET_ME } from '../utils/queries';
