ebf0652942a2e3543a4cbf5503ecab8d35dc6c3c
AVue2 <Vuey00@gmail.com>
2025-04-09 22:05:05 -0400

    modified server.ts file to remove path.join

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,11 @@
 import express, { Request, Response } from 'express';
-import path from 'path';
 import db from './config/connection.js';
 // import routes from './routes/index.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { authenticateToken } from './services/auth.js';
 import { typeDefs, resolvers } from './schemas/index.js';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
@@ -34,10 +30,10 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static('../client/dist'));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+      res.sendFile( '../../client/dist/index.html');
     });
   }
 
